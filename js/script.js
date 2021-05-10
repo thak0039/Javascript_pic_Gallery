bf5c721e4475e31246d67fee523e3036a863abdd
thak0039 <thak0039@algonquinlive.com>
2021-05-10 14:49:27 -0400

    odd even concept

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ function tectchangefun() {
 function changeimage(path) {
     document.getElementById('img-gallarey').src = path;
 }
+
+function GetoOddEvenNumber() {
+    let numbertype = document.getElementById('Idfornumber').value;
+    document.getElementById('text-number').style.color = 'blue';
+    if (numbertype % 2 == 0) {
+
+        let a = numbertype + " " + "is Even ";
+
+        document.getElementById('text-number').innerHTML = a;
+
+    } else {
+        let b = numbertype + " " + "is Odd ";
+        document.getElementById('text-number').innerHTML = b;
+    }
+
+}
